57967b8224b80cc3086ec469b2d4e648efba1db7
Matthias Johann Kurs <mattkur@greeny.cs.tlu.ee>
2015-09-30 12:49:37 +0300

    Car plates

--- data.php
+++ data.php
@@ -11,7 +11,27 @@
 		session_destroy();
 		header("Location: login.php");
 	}
+   $number_plate= $color= "";
+   $number_plate_error=$color_error= "";
+   
+   if(isset($_POST["add_plate"])){
+	   echo "vajutati nuppu";
+	   
+	   
+   }
    
  ?> 
  <p>Tere, <?=$_SESSION["logged_in_user_email"];?>
 	<a href="?logout=1"> Logi välja <a>
+</p>
+
+<h2>Lisa autonumbrimärk </h2>
+<form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>" method="post" >
+	<label for="number_plate">Auto numbrimärk</label><br>
+  	<input id="number_plate" name="number_plate" type="text"  value="<?php echo $number_plate; ?>"> <?php echo $number_plate_error; ?><br><br>
+	<label for="color">Värv</label><br>
+  	<input id="color" name="color" type="text"  value="<?php echo $color; ?>"> <?php echo $color_error; ?><br><br>
+  	<input type="submit" name="add_plate" value="Salvesta">
+  </form>	
+	
+	
